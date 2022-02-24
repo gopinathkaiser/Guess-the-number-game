45fecd4a245700240b594a6c19a35c58173a7e84
gopinathjs <gopinathkaiser@gmail.com>
2022-02-24 12:40:47 +0530

    display status (high or low or equal)

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ window.onload = function () {
 
 function playGame(){
     let numberGuess = document.getElementById("number-guess").value;
-    console.log(correctNumber);
-   displayResult(numberGuess);
+    
+    displayResult(numberGuess);
 }
 
 function displayResult(numberGuess){
     if(numberGuess > correctNumber){
-        console.log("guess too high");
+        showNumberAbove();
     }else if(numberGuess < correctNumber){
-        console.log("guess too low");
+        showNumberBelow();
     }else{
-        console.log("guess is correct");
+        showYouWon();
     }
 }
 
@@ -31,3 +31,41 @@ function getRandomNumber(){
 function initGame(){
 
 }
+
+
+function getDialog(dialogType, text) {
+  let dialog;
+  switch (dialogType) {
+    case "warning":
+      dialog = "<div class='alert alert-warning' role='alert'>";
+      break;
+    case "won":
+      dialog = "<div class='alert alert-success' role='alert'>";
+      break;
+  }
+  dialog += text;
+  dialog += "</div>";
+  return dialog;
+}
+
+function showYouWon() {
+    const text = "Awesome job, you got it!";
+  
+    let dialog = getDialog("won", text);
+    console.log(dialog);
+    document.getElementById("result").innerHTML = dialog;
+  }
+  
+function showNumberAbove() {
+    const text = "Your guess is too high!";
+    
+    let dialog = getDialog("warning", text);
+    document.getElementById("result").innerHTML = dialog;
+  }
+  
+function showNumberBelow() {
+    const text = "Your guess is too low!";
+   
+    let dialog = getDialog("warning", text);
+    document.getElementById("result").innerHTML = dialog;
+  }
